1fbebf29f5e6094bebc9cdb4ee56722ac2f37b43
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-07-12 14:34:19 +0200

    Change the fire effect duration to 5 seconds

--- src/main/java/com/supermartijn642/structureblueprinter/LandmineEffect.java
+++ src/main/java/com/supermartijn642/structureblueprinter/LandmineEffect.java
@@ -140,7 +140,7 @@ public interface LandmineEffect {
         world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(pos).grow(0.7))
             .forEach(entity -> {
                 if(!(entity instanceof EntityPlayer && ((EntityPlayer)entity).isCreative()))
-                    entity.setFire(60);
+                    entity.setFire(5);
             });
     };
 
