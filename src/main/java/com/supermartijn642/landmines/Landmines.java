a0742cfd86df7ce531dfcd3e7289824bd4ba212c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-12-29 22:17:25 +0100

    Update to 1.21.11

--- src/main/java/com/supermartijn642/landmines/Landmines.java
+++ src/main/java/com/supermartijn642/landmines/Landmines.java
@@ -6,7 +6,7 @@ import com.supermartijn642.core.registry.RegistrationHandler;
 import com.supermartijn642.core.registry.RegistryEntryAcceptor;
 import com.supermartijn642.landmines.generators.*;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 
 /**
@@ -34,7 +34,7 @@ public class Landmines implements ModInitializer {
             handler.registerItemCallback(type::registerItem);
         }
         // Trigger sound
-        handler.registerSoundEvent("trigger_sound", SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("landmines", "trigger_sound")));
+        handler.registerSoundEvent("trigger_sound", SoundEvent.createVariableRangeEvent(Identifier.fromNamespaceAndPath("landmines", "trigger_sound")));
     }
 
     private static void registerGenerators(){
--- src/main/java/com/supermartijn642/landmines/generators/LandmineRecipeGenerator.java
+++ src/main/java/com/supermartijn642/landmines/generators/LandmineRecipeGenerator.java
@@ -5,7 +5,7 @@ import com.supermartijn642.core.generator.ResourceCache;
 import com.supermartijn642.landmines.LandmineType;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -23,7 +23,7 @@ public class LandmineRecipeGenerator extends RecipeGenerator {
     @Override
     public void generate(){
         // Pressure plates tag
-        TagKey<Item> pressurePlates = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("landmines", "stone_pressure_plates"));
+        TagKey<Item> pressurePlates = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("landmines", "stone_pressure_plates"));
 
         // Explosion
         this.shaped(LandmineType.EXPLOSIVE.getItem())
